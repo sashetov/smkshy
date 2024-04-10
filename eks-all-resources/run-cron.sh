c442ce898df7f37643910e7a71ee3b5044ee648d
Alex Vassilevski <alexander@vassilevski.com>
2024-04-10 14:13:12 -0400

    updating help/clarity text and run-cron script

--- eks-all-resources/run-cron.sh
+++ eks-all-resources/run-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 cd $HOME/src/eks-all-resources || exit 1
-./get-all-api-resources.sh || exit 1
+./k8s-get-api-resouces || exit 1
 git add . || exit 1
 COMMIT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "Update: $COMMIT_TIME" || exit 1
